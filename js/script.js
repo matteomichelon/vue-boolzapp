e2f1f646a05cb622cd19641a2015504c917813e6
michelonmatteo <michelon.matteo89@gmail.com>
2021-04-22 11:50:06 +0200

    aggiunti file collegati tra loro - prima parte struttura web app

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Document Ready */ 
+$( document ).ready( function () {
+
+    //====
+    /* write here if you use jQuery*/
+    //====
+
+} );
+/* end Document Ready */
+
+/* Vue */ 
+
+var app = new Vue(
+    {
+        el: '#root',
+        data:{
+
+        },
+        methods:{
+
+        }
+    }
+);
+
+/* end Vue */
+
+
